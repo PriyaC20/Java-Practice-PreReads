3f9ae72662167ed624198d0fafea8c034502b2eb
Priya <nishapriyachandran@gmail.com>
2022-01-05 21:07:06 +0530

    Initial commit

---
+++ NestedLoop3.java
@@ -0,0 +1,29 @@
+
+public class NestedLoop3 {
+
+	public static void main(String[] args) {
+		
+		int n=4;
+		
+		for(int i=1; i<=n; i++)
+		{
+			if(i==1 || i==n)
+			{
+				for(int j=1; j<=n; j++)
+					System.out.print("*");
+			}
+			else
+			{
+				for(int j=1; j<=n; j++)
+				{
+					if(j==1 || j==n)
+						System.out.print("*");
+					else
+						System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
