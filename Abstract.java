85fb0a8cf73794e1bbb3daf9c4d288f791659f3d
Priya <nishapriyachandran@gmail.com>
2022-01-06 16:21:45 +0530

    Added 1 more program

---
+++ Abstract.java
@@ -0,0 +1,40 @@
+
+abstract class Writer
+{
+	public abstract void write();
+	
+	public void show()
+	{
+		System.out.println("Abstract Class - Non Abstract Method");
+	}
+}
+
+class Pen extends Writer
+{
+	public void write() 
+	{
+		System.out.println("Writing using Pen");
+	}
+}
+
+class Pencil extends Writer
+{
+	public void write() 
+	{
+		System.out.println("Writing using Pencil");
+	}
+}
+public class Abstract {
+
+	public static void main(String[] args)
+	{
+		Writer penObj = new Pen();
+		penObj.write();
+		penObj.show();
+		
+		Writer pencilObj = new Pencil();
+		pencilObj.write();
+		pencilObj.show();
+		
+	}
+}
