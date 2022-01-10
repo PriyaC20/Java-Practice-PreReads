94eece8c0fc75a3411829574acf40edbf1d85216
PriyaC20 <nishapriyachandran@gmail.com>
2022-01-10 14:40:04 +0530

    Added ProcessingChallenge-Procedural

---
+++ ProcessingChallenge-Procedural-4Balls/src/main/java/ProcessingChallenge.java
@@ -0,0 +1,42 @@
+import processing.core.PApplet;
+
+public class ProcessingChallenge extends PApplet {
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    public static final int DIAMETER = 10;
+    private int x = 0;
+
+    public static void main(String[] args) {
+        PApplet.main("ProcessingChallenge", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    public void draw() {
+        process4Balls();
+        incrementX();
+    }
+
+    private void process4Balls() {
+        for (int i = 1; i <=4 ; i++)
+            drawBall(i,i);
+    }
+
+    private void drawBall(int speed, int y) {
+        ellipse(x*speed, HEIGHT*y/5, DIAMETER, DIAMETER);
+    }
+
+    private void incrementX() {
+        x++;
+    }
+}
